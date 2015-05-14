a636537db506ffc44a2ad01ac8ec7027e46cbc6b
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-05-14 18:13:50 +0100

    T

---
+++ Start.sh
@@ -0,0 +1,11 @@
+echo Teddy alive v0.1 ALPHA EMULATOR
+echo Preparing...
+echo "Hello, Welcome"
+echo -n Enter the login to use
+read login
+
+export $login
+$loginprompt=$login
+export $loginprompt
+
+echo Please login...
