245765f0bc548ff0694796d8edcbc8a8149b3f7e
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-05-25 07:43:57 +0100

    Create clonetest.java

---
+++ TeddyAlive/src/test/java/com/gumjoe/universalutils/clonetest.java
@@ -0,0 +1,15 @@
+package com.gumjoe.universalutils;
+ 
+import junit.framework.Assert;
+import org.junit.Test;
+ 
+public class CloneTest {
+ 
+	@Test
+	public void testPrintclone() {
+                File bin = new File( "./bin"
+		Assert.assertEquals(universalutils.clone("https://github.com/Gum-Joe/TeddyAlive.git", "master" , bin), "Cloning into ./bin");
+ 
+	}
+ 
+}
